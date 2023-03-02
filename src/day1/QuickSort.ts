c5270a33a7283bcf7c6effb6786716874f1afd84
Ryan Van Valkenburg <ryanvanvalkenburg@gamestop.com>
2023-03-01 21:13:48 -0800

    Quicksort start

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,25 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {}
 
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; ++i) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+        }
+    }
+    return 0;
 }
+
+export default function quick_sort(arr: number[]): void {
+    if (arr.length <= 1) return;
+
+    qs(arr, 0, arr.length - 1);
+}
+
+// bestcase: o(nlogn)
+// worstcase: o(n^2)
+~
